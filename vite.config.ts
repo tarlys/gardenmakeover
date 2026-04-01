2436d1db962d7f3459db14f5d83a2c5a79e10093
Taras Lysenko <taras.lysenko@ilogos.biz>
2026-04-01 12:41:26 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ export default defineConfig({
   server: {
     open: true,
   },
+  base: './',
+  build: {
+    sourcemap: false,
+    cssCodeSplit: false,
+    modulePreload: false,
+    target: 'es2018',
+  },
 })
